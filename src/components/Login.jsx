d1935a0b36a749589ec4dd40e18f26b34b943c7f
noammery <noammery55@gmail.com>
2022-11-10 11:08:39 +0200

    stracture

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+export const Login = () => {
+  return (
+    <div>
+      <div className="h-screen">
+        <h1>התחברות</h1>
+      </div>
+      <div className="h-screen"></div>
+    </div>
+  );
+};
