73ec929d145a9ac2dad2c098d9d9c4fa5dba6118
yohaiprins <yohaiprins33@gmail.com>
2022-11-16 12:17:02 +0200

    update

--- src/components/Department.jsx
+++ src/components/Department.jsx
@@ -1,57 +1,77 @@
 import React from "react";
-import axios from 'axios'
+import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
-
-
-
+import { useLocation } from "react-router";
 
 function Department() {
- 
-  // const [userdepartmentData, setUserdepartmentData] = useState();
-  // useEffect(() => {
-  // axios.post("http://localhost:5000/auth/userbydepartment").then((res) => setsetUserdepartmentData(res.data))
-  // },[])
-  
- 
-  
+  const [postData, setpostData] = useState();
+  const [userData, setuserData] = useState();
+  const location = useLocation();
+  const { department } = location.state;
 
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/departments/data", {
+        department: department,
+      })
+      .then((res) => setpostData(res.data));
+  }, [department]);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/auth/userbydepartment", {
+        department: department,
+      })
+      .then((res) => setuserData(res.data));
+  }, [department]);
 
   return (
-    <div className="mt-24">
-      <h1 className="text-5xl flex justify-center mb-10 font-semibold text-gray-900">
-        שם אגף
+    <div>
+      <h1 className="text-5xl flex justify-center mb-10 font-semibold text-gray-900 mt-36">
+        {department}
       </h1>
-      <div className="flex justify-center ">
-        <div className="text-xl flex justify-items-center justify-center flex-wrap w-4/6 flex-col ">
-          <div className="  text-center">
-            <h1>כותרת לכתבה</h1>
-          </div>
-          <div className="mr-4 mt-4">
-            <p>
-              פוסט עם מלא תיאור ומילים ומשפטים ושטויות ויופע בו גם תאריך ונוסיף
-              תמונה
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center w-2/6 ">
-          <h1 className="mb-5 text-2xl font-medium">העובדים באגף</h1>
-          <ul>
-            <li className="m-2">
-              <div className="flex justify-between w-40 items-center flex-row-reverse ">
+      <div className="mt-24 ">
+        <div className="flex justify-center">
+          {postData?.map((post, index) => {
+            return (
+              <div
+                key={index}
+                className="bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 text-center justify-end w-96 mt-6 mr-10"
+              >
+                <h1>{post.title}</h1>
+                <h1>{post.description}</h1>
+                <h1>{post.date}</h1>
                 <img
-                  src="https://pps.whatsapp.net/v/t61.24694-24/215742736_169130348833804_5618789588749101516_n.jpg?ccb=11-4&oh=01_AdSC1WCVuXHYd2xh_nnwFPNIXNjqdpTvwP8dIfcHPj8IBw&oe=6379D759"
-                  className="w-14 rounded-full"
-                  alt=""
+                  className="w-20   object-fill"
+                  src={post.image}
+                  alt="Sunset in the mountains"
                 />
-                <h1>Noam Mery</h1>
               </div>
-            </li>
-          </ul>
+            );
+          })}
+        </div>
+        <h1 className="mt-10 text-center">עובדים באגף</h1>
+        <div className="flex justify-center mt-14">
+          {userData?.map((user, index) => {
+            return (
+              <div
+                key={index}
+                className="bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 text-center justify-end w-96 mb-6"
+              >
+                <h1>{user.fullname}</h1>
+                <img
+                  className="w-20   object-fill"
+                  src={user.image}
+                  alt="Sunset in the mountains"
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Department;
