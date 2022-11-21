bfa3b9d5c673dd51d9ae169f6425df4db582f3b4
noammery <noammery55@gmail.com>
2022-11-21 15:07:03 +0200

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,8 +30,9 @@ export const Home = () => {
     axios
       .get(`${process.env.REACT_APP_SERVER}/auth/users`, usersBirth)
       .then((res) => console.log(res.data));
+    // eslint-disable-next-line
   }, []);
-  
+
   return (
     <div className="bg-gray-200 min-h-screen">
       {modal && (
@@ -86,20 +87,20 @@ export const Home = () => {
           </button>
         </div>
         <div className="p-10">
-        <div className="w-screen  flex flex-col items-center">
-          <button onClick={handleOpen}>הצג</button>
-          {open ? (
-        <ul className="menu">
-          <li className="menu-item">
-            <button>Menu 1</button>
-          </li>
-          <li className="menu-item">
-            <button>Menu 2</button>
-          </li>
-        </ul>
-      ) : null}
-          {open ? <div>חוגגים החודש</div> : <div>חוגגים החודש</div>}
-        </div>
+          <div className="w-screen  flex flex-col items-center">
+            <button onClick={handleOpen}>הצג</button>
+            {open ? (
+              <ul className="menu">
+                <li className="menu-item">
+                  <button>Menu 1</button>
+                </li>
+                <li className="menu-item">
+                  <button>Menu 2</button>
+                </li>
+              </ul>
+            ) : null}
+            {open ? <div>חוגגים החודש</div> : <div>חוגגים החודש</div>}
+          </div>
           {events?.map((post, index) => {
             return (
               <div
